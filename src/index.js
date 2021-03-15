07fc3fc0068ce30399a950f64ae9f317f5af97a2
denchik3192 <den465341@gmail.com>
2021-03-15 18:44:18 +0300

    feat: implement 3 functions: max, min, and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
+
+if(array === undefined || array.length == 0){
   return 0;
+}else{
+  return Math.min.apply(null, array)
+}
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+
+  if(array === undefined || array.length == 0){
+    return 0;
+  }else{
+    return Math.max.apply(null, array);
+  }
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length == 0){
+    return 0;
+  }
+  let total = array.reduce(function(a, b) {
+    return a + b;
+  });
+  let avg =  total / array.length
+  
+return avg;
+
+  // return 0;
 }
